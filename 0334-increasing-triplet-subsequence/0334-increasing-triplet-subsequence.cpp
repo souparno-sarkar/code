bb18ca0a4f647a025f5fa1544e465aceb64d4c0c
souparno-sarkar <souparnosarkarobra@gmail.com>
2026-05-30 12:10:24 +0530

    Time: 1 ms (49.85%), Space: 115.6 MB (67.05%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
-    bool des_sort(vector<int>& arr , vector<int>& nums)
-    {
-        sort(arr.begin() , arr.end());
-        reverse(arr.begin() , arr.end());
-        return (arr == nums);
-    }
-    bool check(vector<int>& nums)
-    {
-        set<int> st(nums.begin() , nums.end());
-        return (st.size() < 3);
-    }
     bool increasingTriplet(vector<int>& nums) 
     {
-        int n = nums.size();
-        vector<int> arr = nums;
-        if(check(nums))
-            return false;
-        if(des_sort(arr , nums))
-            return false;
-        else
+        int first = INT_MAX;
+        int sec = INT_MAX;
+        for(int i : nums)
         {
-            for(int i=0;i<n;i++)
-            {
-                for(int j=i;j<n;j++)
-                {
-                    if(nums[i] < nums[j])
-                    {
-                        for(int k=j;k<n;k++)
-                        {
-                            if(nums[j] < nums[k])
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;    
+            if(i <= first)
+                first = i;
+            else if(i <= sec)
+                sec = i;
+            else
+                return true;
+        }   
+        return false;
     }
 };
