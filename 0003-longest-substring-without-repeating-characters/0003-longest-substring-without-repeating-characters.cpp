ec481f1eddcb40d7a94ae587fc410099a4f8dc2c
souparno-sarkar <souparnosarkarobra@gmail.com>
2026-07-28 00:50:46 +0530

    Time: 5 ms (68.28%), Space: 12.3 MB (39.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,49 +1,25 @@
-/*Notes : Take a hash table and then insert the element as per given string 
-        Check wheteher the alphabets are repeting or not
-        if repeting then check that the index of the repeated alphabet is >= left then update 
-        left = index+1;
-*/
-
-#include<bits/stdc++.h>
-#define vi vector<int>
-#define um_i unordered_map<int , int>
-#define um_c unordered_map<char , int>
-#define co cout <<
-#define ci cin >>
-using ll = long long;
-using namespace std;
-
-class Solution 
-{
+class Solution {
 public:
-    int lengthOfLongestSubstring(string s) 
-    {
-        if(s.size() == 0 || s.size() == 1)
-            return s.size();
-
-        um_c mp;
-
-        int str = 0 , end = 0;
-        int max_size = 0;
-
-        while(end < s.size())
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char , int> mp;
+        int max_seq = 0;
+        int n = s.size();
+        int l = 0;
+        int r = 0;
+        while(r < n)
         {
-            if(mp.find(s[end]) == mp.end())
+            mp[s[r]]++;
+            if(mp.size() == r-l+1)
+                max_seq = max(max_seq , r-l+1);
+            else if(mp.size() < r-l+1)
             {
-                mp[s[end]]++;
-                end++;
+                mp[s[l]]--;
+                if(mp[s[l]] == 0)
+                    mp.erase(s[l]);
+                l++;
             }
-            else
-            {
-                max_size = max(max_size , (int)mp.size());
-                mp.clear();
-                str++;
-                end = str;
-            }
+            r++;
         }
-
-        max_size = max(max_size , (int)mp.size());
-
-        return max_size;
+        return max_seq;
     }
 };
